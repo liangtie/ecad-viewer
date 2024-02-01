794d4b5795992b843b25920726c61b7c0097ff29
Ethan Chien <liangtie.qian@gmail.com>
2024-02-01 14:50:10 +0800

    RM vrml viewer

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,6 @@ import "./ecad-viewer/utils/ecad_source";
 import "./ecad-viewer/utils/set_up_sprite";
 import "./ecad-viewer/view/kicad_footprint_viewer";
 import "./ecad-viewer/view/kicad_symbol_viewer";
-import "./ecad-viewer/view/vrml_viewer";
-import "./ecad-viewer/view/step_viewer";
 import "./kc-ui/select";
 import "./newFile";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
