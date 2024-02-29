13edd86442c971a6518220bfe6e981f6f61e1aa8
Ethan Chien <liangtie.qian@gmail.com>
2024-02-29 19:00:24 +0800

    Add selection menu

---
+++ src/kicanvas/elements/kc-board/selection-menu.ts
@@ -0,0 +1,11 @@
+import { KCUIElement } from "../../../kc-ui/element";
+
+export class SelectionMenu extends KCUIElement {
+    private readonly menu: HTMLDivElement;
+    private readonly menu_items: HTMLDivElement;
+    private readonly menu_item_template: HTMLDivElement;
+    private readonly menu_item_template_text: HTMLDivElement;
+    private readonly menu_item_template_icon: HTMLDivElement;
+    private readonly menu_item_template_icon_text: HTMLDivElement;
+    private readonly menu_item_template_icon_text_text: HTMLDivElement;
+}
